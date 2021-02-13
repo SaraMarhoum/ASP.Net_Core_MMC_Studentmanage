0b625d9cd7ec5101cd1bc092e73fe996fc6cc2c5
SaraMarhoum <57989282+SaraMarhoum@users.noreply.github.com>
2021-02-13 12:02:42 +0100

    Add files

---
+++ ASPcrudMVC_Student/ASPcrudMVC_Student/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ASPcrudMVC_Student.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<NewSudentClass> tablestd { get; set; }
+    }
+}
